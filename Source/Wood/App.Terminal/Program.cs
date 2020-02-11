33f51cb549a8b9c91fb33f3458270f7f1d68155e
Guillermo Palma <gapalmasolano@gmail.com>
2020-02-11 15:14:33 -0700

    Add Initial Code

---
+++ Source/Wood/App.Terminal/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace App.Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cortesH = Triplay.Cut.Horizontal(12, 15);
+            int cortesV = Triplay.Cut.Vertical(12, 15);
+
+            int countHorizontal = 0, countVertical = 0;
+            double PiezaHorizontal = 12, PiezaVertical = 15;
+
+            double MedidaVertical = 48, MedidaHorizontal = 96, octa = .125;
+
+            while (PiezaHorizontal <= MedidaHorizontal)
+            {
+                MedidaHorizontal = (MedidaHorizontal - PiezaHorizontal)  - octa;
+                countHorizontal++;
+            }
+
+            while (PiezaVertical <= MedidaVertical)
+            {
+                MedidaVertical = (MedidaVertical - PiezaVertical) - octa;
+                countVertical++;
+            }
+
+            //while (PiezaVertical < MedidaHorizontal & PiezaHorizontal < MedidaVertical)
+            //{
+            //    MedidaHorizontal = (MedidaHorizontal - PiezaVertical) - (1/8);
+            //    MedidaVertical = (MedidaVertical - PiezaHorizontal) - (1/8);
+            //    countVertical++;
+            //}
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Source/Wood/App.Terminal/Triplay/Cut.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Terminal.Triplay
+{
+    public class Cut
+    {
+        public static int Horizontal(double PiezaHorizontal, double PiezaVertical)
+        {
+            int countHorizontal = 0, countVertical = 0;
+            //double PiezaHorizontal = 12, PiezaVertical = 15;
+
+            double MedidaVertical = 48, MedidaHorizontal = 96, octa = .125;
+
+            while (PiezaHorizontal <= MedidaHorizontal)
+            {
+                MedidaHorizontal = (MedidaHorizontal - PiezaHorizontal) - octa;
+                countHorizontal++;
+            }
+            while (PiezaVertical <= MedidaVertical)
+            {
+                MedidaVertical = (MedidaVertical - PiezaVertical) - octa;
+                countVertical++;
+            }
+            return countHorizontal * countVertical;
+        }
+
+        public static int Vertical(double PiezaHorizontal, double PiezaVertical)
+        {
+            int countHorizontal = 0, countVertical = 0;
+            //double PiezaHorizontal = 12, PiezaVertical = 15;
+
+            double MedidaVertical = 48, MedidaHorizontal = 96, octa = .125;
+
+            while (PiezaHorizontal <= MedidaHorizontal)
+            {
+                MedidaHorizontal = (MedidaHorizontal - PiezaVertical) - octa;
+                countHorizontal++;
+            }
+            while (PiezaVertical <= MedidaVertical)
+            {
+                MedidaVertical = (MedidaVertical - PiezaHorizontal) - octa;
+                countVertical++;
+            }
+            return countHorizontal * countVertical;
+        }
+    }
+}
